6a9dabdf408e92d3ecd6166bd2fb787e91f023c6
siddhantDeval <siddhantdeval15@gmail.com>
2023-10-08 01:34:33 +0530

    Custom Diractive  * render2

--- data-binding/src/app/CustomDiractives/quotes.diractive.ts
+++ data-binding/src/app/CustomDiractives/quotes.diractive.ts
@@ -1,11 +1,18 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[txtPri]',
 })
 export class TestColor implements OnInit {
-  constructor(private element: ElementRef) {}
+  constructor(
+    private element: ElementRef,
+    private render2: Renderer2
+  ) {}
   ngOnInit() {
     const p = this.element.nativeElement as HTMLParagraphElement;
-    p.classList.add('text-primary');
+    /*one way ,not a best practice
+     p.classList.add('text-primary'); */
+
+    //second way , good practice
+    this.render2.addClass(p, 'text-primary');
   }
 }
